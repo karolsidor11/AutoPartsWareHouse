7d9cb6aa56fbd6c2dd64ae25472ef9bb9f5a2d93
KursJava <kursjava1@wp.pl>
2019-05-01 12:39:16 +0200

    add user and role table

---
+++ src/main/resources/static/user.sql
@@ -0,0 +1,9 @@
+INSERT INTO ROLE VALUES
+  (1, 'ADMIN'),
+  (2, 'USER');
+
+INSERT INTO UŻYTKOWNIK VALUES
+  (1, 'karolsidor11@wp.pl', 'Sidor', 'Karol', 1),
+  (2, 'nowak@wp.pl', 'Nowak', 'Jan', 2);
+
+
